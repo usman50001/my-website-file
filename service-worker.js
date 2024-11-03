5b7885e9f15adf3c6e99e18dd84a3cfe8f37e585
usman50001 <janjuasellers1@gmail.com>
2024-11-02 22:24:07 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
